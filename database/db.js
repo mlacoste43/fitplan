3900ec1b33f30e62efa5545a69f261a46b07efd0
mlacoste43 <maksimwilman@gmail.com>
2026-06-02 17:50:06 +0300

    Update db.js

--- database/db.js
+++ database/db.js
@@ -3,7 +3,7 @@ const { Pool } = pg;
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: false,
+  ssl: { rejectUnauthorized: false },
 });
 
 // ── Инициализация таблиц ──────────────────────────────────────────────────────
